e3911c481fb3b935332aa0e7bc984971dad3f0d5
NmeZxc <vechira999@outlook.com>
2025-03-28 11:51:47 +0200

    Update hooks.hpp
    Add __try/__except

--- hooks.hpp
+++ hooks.hpp
@@ -7,7 +7,10 @@
 //WND Proc event hook.
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	//Using base pointer arithmetic.
-	*((float*)(GTABase + GTA_ScopeMouseY)) = ((*(float*)(GTABase + GTA_ScopeMouseX)) / 2);
+	__try{
+		*((float*)(GTABase + GTA_ScopeMouseY)) = ((*(float*)(GTABase + GTA_ScopeMouseX)) / 2);
+	}
+	__except(1){}
 	//Return original
 	return CallWindowProcA(oWndProc, hWnd, msg, wParam, lParam);
 }
